6786b9a79aab3ae076ca08544f13dc10cf9febc4
ReKuShE <70316694+IRAQ-hacker@users.noreply.github.com>
2022-09-16 15:32:49 +0300

    Update YOUTUBE_downloads.py

--- YOUTUBE_downloads.py
+++ YOUTUBE_downloads.py
@@ -2,7 +2,7 @@ import os
 import telebot
 import youtube_dl
 
-bot = telebot.TeleBot("token")
+bot = telebot.TeleBot("5144279583:AAFhgkrRLWm1xw5b4Z-P_f5-6kPS8S_weTY")
 
 
 @bot.message_handler(commands=['start'])
